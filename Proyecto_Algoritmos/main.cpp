e4182a9cc2969d8917ba4acb60fa443640368690
Fabricio-A-E <fabricioae21@gmail.com>
2020-06-23 00:04:04 -0600

    Corregir el Iniciar Sesion y cambié a puntero todas las listas y vectores de business, data, clase grande

--- Proyecto_Algoritmos/main.cpp
+++ Proyecto_Algoritmos/main.cpp
@@ -11,34 +11,36 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-//    Grafo grafo;
-//    grafo.agregarAristaYPeso(new Vertice(new Pais("CR")), new Vertice(new Pais("PN")), 10);
-////    grafo.agregarAristaYPeso(new Vertice(new Pais("EU")), new Vertice(new Pais("PN")), 52);
-////    grafo.agregarAristaYPeso(new Vertice(new Pais("PN")), new Vertice(new Pais("COL")), 82);
-//    grafo.agregarAristaYPeso(new Vertice(new Pais("CR")), new Vertice(new Pais("GM")), 152);
-//    grafo.agregarAristaYPeso(new Vertice(new Pais("CR")), new Vertice(new Pais("COL")), 152);
-//    cout<<grafo.toString()<<endl;
-//    grafo.moverVertices(0);
-//    grafo.moverVertices(1);
-//    grafo.moverVertices(2);
-//    grafo.moverVertices(3);
-//    grafo.moverVertices(4);
-//    grafo.moverVertices(5);
-//    //        
-//                Glib::RefPtr<Gtk::Application> app =
-//                        Gtk::Application::create(argc, argv, "flag2");
-//            
-//                VentanaPrincipal ventana;
-//                ventana.set_title("Ventana Principal");
-//            
-//                return app->run(ventana);
+    //    Grafo grafo;
+    //    grafo.agregarAristaYPeso(new Vertice(new Pais("CR")), new Vertice(new Pais("PN")), 10);
+    ////    grafo.agregarAristaYPeso(new Vertice(new Pais("EU")), new Vertice(new Pais("PN")), 52);
+    ////    grafo.agregarAristaYPeso(new Vertice(new Pais("PN")), new Vertice(new Pais("COL")), 82);
+    //    grafo.agregarAristaYPeso(new Vertice(new Pais("CR")), new Vertice(new Pais("GM")), 152);
+    //    grafo.agregarAristaYPeso(new Vertice(new Pais("CR")), new Vertice(new Pais("COL")), 152);
+    //    cout<<grafo.toString()<<endl;
+    //    grafo.moverVertices(0);
+    //    grafo.moverVertices(1);
+    //    grafo.moverVertices(2);
+    //    grafo.moverVertices(3);
+    //    grafo.moverVertices(4);
+    //    grafo.moverVertices(5);
     
-        
+    ClaseGrande * claseGrande = ClaseGrande::getInstance();
+    Glib::RefPtr<Gtk::Application> app =
+            Gtk::Application::create(argc, argv, "flag2");
+    
+    VentanaPrincipal ventana;
+    ventana.set_title("Ventana Principal");
+    
+    return app->run(ventana);
 
-                ClaseGrande * claseGrande = ClaseGrande::getInstance();
+
+    /*
                 
-                claseGrande->registrarCliente(new Client("12","Nombre","Genero","Pasaporte","contra",new Pais("CR")));
-                cout<<claseGrande->searchClient("Pasaporte","contra")<<endl;
+                
+                    claseGrande->registrarCliente(new Client("12","Nombre","Genero","Pasaporte","contra",new Pais("CR")));
+                    cout<<(claseGrande->searchClient("Pasaporte","contra"))->toString()<<endl;
+     */
     //Aerolinea* aerolinea=new Aerolinea("A");
     //cout<<"1"<<((Itinerario*)aerolinea->getItinerarios().front())->toString()<<endl;
     //aerolinea->arribaItinerarios();
